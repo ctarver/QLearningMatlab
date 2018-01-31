ca496335bc7907e6c035ff031b38d2a45246b6c7
Chance <tarver.chance@gmail.com>
2018-01-31 10:42:14 -0600

    Make code more modular

--- QLearning.m
+++ QLearning.m
@@ -5,9 +5,7 @@ classdef QLearning
    %environment.
    
    properties
-      nStates
-      nActions
-      environment
+      markovDecisionProcess
       QMatrix
       RMatrix
       gamma
@@ -18,77 +16,111 @@ classdef QLearning
    end
    
    methods
-      function obj = QLearning(nStates, nActions, gamma, RMatrix, ...
-            initialState, environment)
+      function obj = QLearning(markovDecisionProcess, gamma, RMatrix, ...
+            initialState, learningRate)
          %QLEARNING Construct an instance of this class
          %   Detailed explanation goes here
          
-         learningRate = 0.5;
          if nargin == 0
-            obj.nStates = 5;
-            obj.nActions = 2;
+            obj.learningRate = 0.5;
+            obj.markovDecisionProcess.nStates = 5;
+            obj.markovDecisionProcess.nActions = 2;
             obj.gamma = 0.5;
             
-            obj.RMatrix = zeros(obj.nStates, obj.nActions);
-            obj.currentState = randi(obj.nStates);
-            obj.environment = randi([1 obj.nStates], obj.nStates,  ...
-               obj.nActions);
-            stateWithoutRewards = randi(obj.nStates);
+            obj.RMatrix = zeros(obj.markovDecisionProcess.nStates, ...
+               obj.markovDecisionProcess.nActions);
+            obj.currentState = randi(obj.markovDecisionProcess.nStates);
+            obj.markovDecisionProcess.environment = randi([1 ...
+               obj.markovDecisionProcess.nStates], ...
+               obj.markovDecisionProcess.nStates,  ...
+               obj.markovDecisionProcess.nActions);
+            stateWithoutRewards = randi(obj.markovDecisionProcess.nStates);
             obj.RMatrix(stateWithoutRewards,:) = 0;
-            undesirableState = randi(obj.nStates);
+            undesirableState = randi(obj.markovDecisionProcess.nStates);
             obj.undesirableState = undesirableState;
-            obj.RMatrix(obj.environment==undesirableState) = -100;
-            desirableState = randi(obj.nStates);
+            obj.RMatrix(obj.markovDecisionProcess.environment==...
+               undesirableState) = -100;
+            desirableState = randi(obj.markovDecisionProcess.nStates);
             while desirableState == undesirableState
-               desirableState = randi(obj.nStates);
+               desirableState = randi(obj.markovDecisionProcess.nStates);
             end
             obj.desirableState = desirableState;
-            obj.RMatrix(obj.environment==desirableState) = 100;
+            obj.RMatrix(obj.markovDecisionProcess.environment==...
+               desirableState) = 100;
          else
-            obj.nStates = nStates;
-            obj.nActions = nActions;
+            obj.markovDecisionProcess.nStates = ...
+               markovDecisionProcess.nStates;
+            obj.markovDecisionProcess.nActions = ...
+               markovDecisionProcess.nActions;
             obj.gamma = gamma;
             obj.RMatrix = RMatrix;
             obj.currentState = initialState;
-            obj.environment = environment;
+            obj.learningRate = learningRate;
          end
          
-         obj.QMatrix = zeros(obj.nStates, obj.nActions);
-         nIterations = 500;
+         obj = obj.initializeQMatrix();
+         obj = obj.performQLearning();
+         
+      end
+      
+      function obj = initializeQMatrix(obj)
+         %initializeQMatrix We set up the Q table by initializing all
+         %elements to be equal to 0
+         %
+         % Syntax: obj = obj.initializeQMatrix();
+         
+         obj.QMatrix = zeros(obj.markovDecisionProcess.nStates, ...
+            obj.markovDecisionProcess.nActions);
+      end
+      
+      function obj = performQLearning(obj,nIterations,explorationRate)
+         %performQLearning Performs the Q learning on the object
+         %
+         % Syntax: obj = obj.performQLearning(nIterations,explorationRate);
+         %
+         % Inputs:
+         %   nIterations     - Number of iterations used to perform Q learning
+         %   explorationRate - Rate at which a random action is taken. Must
+         %                     be between 0 and 100.
+         if nargin == 1
+            nIterations = 500;
+            explorationRate = 50;
+         end
+         
+         % Set up variables for analysis
          arrayForm = zeros(length(obj.QMatrix(:)),nIterations);
          stateArray = zeros(1,nIterations);
-         explorationRate = 50;
-         for i = 1:nIterations
-            %Choose an action.
-            [~, nextAction] = max(obj.QMatrix(obj.currentState,:));
+         
+         % Start Iterations
+         for i = 1:nIterations            
+            % Choose an action either through random exploration or
+            % according to the Q policy
             if randi(100) > explorationRate
-               nextAction = randi(obj.nActions);
+               nextAction = randi(obj.markovDecisionProcess.nActions);
+            else
+               [~, nextAction] = max(obj.QMatrix(obj.currentState,:));
             end
-            %Get next state
-            nextState = obj.environment(obj.currentState,nextAction);
+            % Lookup next state for the current state and choosen action
+            nextState = obj.markovDecisionProcess.environment(obj.currentState,nextAction);
             
             %Update Q Matrix
             obj.QMatrix(obj.currentState, nextAction) = ...
-               (1-learningRate)*...
+               (1-obj.learningRate)*...
                obj.QMatrix(obj.currentState, nextAction) + ...
-               learningRate*(obj.RMatrix(obj.currentState,nextAction) + ...
+               obj.learningRate*(obj.RMatrix(obj.currentState,nextAction) + ...
                obj.gamma * max(obj.QMatrix(nextState,:)));
             arrayForm(:,i) = obj.QMatrix(:);
             stateArray(i) = nextState;
             %Update current state
             obj.currentState = nextState;
          end
+         
+         %Figures
          figure
          plot(arrayForm');
          figure
          plot(stateArray);
          obj
-      end
-      
-      function outputArg = method1(obj,inputArg)
-         %METHOD1 Summary of this method goes here
-         %   Detailed explanation goes here
-         outputArg = obj.Property1 + inputArg;
       end
    end
 end
